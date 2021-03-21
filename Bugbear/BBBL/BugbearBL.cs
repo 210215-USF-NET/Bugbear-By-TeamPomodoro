8170e9f46e58f9b2ec89afc92196d9e24c359fdf
ZMFrench <inbox@zachfrench.com>
2021-03-20 23:13:47 -0400

    Initial Creation of BL, DL, and MVC folders.  Creation of Add, Get, and Delete methods in Interfaces.

---
+++ Bugbear/BBBL/BugbearBL.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace BBBL
+{
+    public class BugbearBL : IBugbearBL
+    {
+    }
+}
---
+++ Bugbear/BBModels/Campaign.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BBModels
+{
+    public class Campaign
+    {
+        public int CampaignID { get; set; }
+        public string CampaignName { get; set; }
+        public string Description { get; set; }
+        public int UserID { get; set; }//perhaps this should be "GameMasterID"?
+    }
+}
