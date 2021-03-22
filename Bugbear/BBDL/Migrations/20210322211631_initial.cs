96d361ad6d9b1d0628e6a04f87d0bc05994251e9
ZMFrench <inbox@zachfrench.com>
2021-03-22 17:28:45 -0400

    Database Established

---
+++ Bugbear/BBDL/Migrations/20210322211631_initial.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace BBDL.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Campaigns",
+                columns: table => new
+                {
+                    CampaignID = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    CampaignName = table.Column<string>(type: "text", nullable: true),
+                    Description = table.Column<string>(type: "text", nullable: true),
+                    GameMasterID = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Campaigns", x => x.CampaignID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Characters",
+                columns: table => new
+                {
+                    CharacterID = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    CharacterName = table.Column<string>(type: "text", nullable: true),
+                    Money = table.Column<int>(type: "integer", nullable: false),
+                    UserID = table.Column<int>(type: "integer", nullable: false),
+                    HP = table.Column<int>(type: "integer", nullable: false),
+                    XPLevel = table.Column<int>(type: "integer", nullable: false),
+                    Strength = table.Column<int>(type: "integer", nullable: false),
+                    Dexterity = table.Column<int>(type: "integer", nullable: false),
+                    Constitution = table.Column<int>(type: "integer", nullable: false),
+                    Intelligence = table.Column<int>(type: "integer", nullable: false),
+                    Wisdom = table.Column<int>(type: "integer", nullable: false),
+                    Charisma = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Characters", x => x.CharacterID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Encounters",
+                columns: table => new
+                {
+                    EncounterID = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    EncounterTitle = table.Column<string>(type: "text", nullable: true),
+                    EncounterDescription = table.Column<string>(type: "text", nullable: true),
+                    LocationID = table.Column<int>(type: "integer", nullable: false),
+                    CampaignID = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Encounters", x => x.EncounterID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Locations",
+                columns: table => new
+                {
+                    LocationID = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    LocationName = table.Column<string>(type: "text", nullable: true),
+                    LocationDescription = table.Column<string>(type: "text", nullable: true),
+                    CampaignID = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Locations", x => x.LocationID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Maps",
+                columns: table => new
+                {
+                    MapID = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    MapTitle = table.Column<string>(type: "text", nullable: true),
+                    MapImage = table.Column<byte[]>(type: "bytea", nullable: true),
+                    CampaignID = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Maps", x => x.MapID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "NPCs",
+                columns: table => new
+                {
+                    NPCID = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    NPCName = table.Column<string>(type: "text", nullable: true),
+                    NPCDescription = table.Column<string>(type: "text", nullable: true),
+                    CampaignID = table.Column<int>(type: "integer", nullable: false),
+                    HP = table.Column<int>(type: "integer", nullable: false),
+                    XPLevel = table.Column<int>(type: "integer", nullable: false),
+                    Strength = table.Column<int>(type: "integer", nullable: false),
+                    Dexterity = table.Column<int>(type: "integer", nullable: false),
+                    Constitution = table.Column<int>(type: "integer", nullable: false),
+                    Intelligence = table.Column<int>(type: "integer", nullable: false),
+                    Wisdom = table.Column<int>(type: "integer", nullable: false),
+                    Charisma = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NPCs", x => x.NPCID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Stories",
+                columns: table => new
+                {
+                    StoryID = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    DateCreated = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    StoryTitle = table.Column<string>(type: "text", nullable: true),
+                    StoryDescription = table.Column<string>(type: "text", nullable: true),
+                    CampaignID = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Stories", x => x.StoryID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    UserID = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    FirstName = table.Column<string>(type: "text", nullable: true),
+                    LastName = table.Column<string>(type: "text", nullable: true),
+                    Email = table.Column<string>(type: "text", nullable: true),
+                    Phone = table.Column<string>(type: "text", nullable: true),
+                    CampaignID = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.UserID);
+                    table.ForeignKey(
+                        name: "FK_Users_Campaigns_CampaignID",
+                        column: x => x.CampaignID,
+                        principalTable: "Campaigns",
+                        principalColumn: "CampaignID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Items",
+                columns: table => new
+                {
+                    ItemID = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    ItemName = table.Column<string>(type: "text", nullable: true),
+                    ItemDescription = table.Column<string>(type: "text", nullable: true),
+                    CharacterID = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Items", x => x.ItemID);
+                    table.ForeignKey(
+                        name: "FK_Items_Characters_CharacterID",
+                        column: x => x.CharacterID,
+                        principalTable: "Characters",
+                        principalColumn: "CharacterID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Items_CharacterID",
+                table: "Items",
+                column: "CharacterID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_CampaignID",
+                table: "Users",
+                column: "CampaignID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Encounters");
+
+            migrationBuilder.DropTable(
+                name: "Items");
+
+            migrationBuilder.DropTable(
+                name: "Locations");
+
+            migrationBuilder.DropTable(
+                name: "Maps");
+
+            migrationBuilder.DropTable(
+                name: "NPCs");
+
+            migrationBuilder.DropTable(
+                name: "Stories");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Characters");
+
+            migrationBuilder.DropTable(
+                name: "Campaigns");
+        }
+    }
+}
