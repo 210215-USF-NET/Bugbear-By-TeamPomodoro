901f0c5283ba620939cf9cb48499f5828da6759b
Jacob Gaughan <jakegaughan5@gmail.com>
2021-03-27 16:37:18 -0500

    updated user info

---
+++ Bugbear/BBDL/Migrations/20210327213651_DBFix.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BBDL.Migrations
+{
+    public partial class DBFix : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Phone",
+                table: "Users");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "Users",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "Users",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Phone",
+                table: "Users",
+                type: "text",
+                nullable: true);
+        }
+    }
+}
--- Bugbear/BBModels/User.cs
+++ Bugbear/BBModels/User.cs
@@ -5,10 +5,6 @@ namespace BBModels
     public class User
     {
         public int UserID { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
         public string Email { get; set; }
-        public string Phone { get; set; }
-
     }
 }
