8e7750d33eaf461b28aa8811e41529347c7300b0
Jacob Gaughan <jakegaughan5@gmail.com>
2021-03-29 14:52:58 -0500

    fix the broken api

---
+++ Bugbear/BBDL/Migrations/20210329195140_RelationshipFix2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BBDL.Migrations
+{
+    public partial class RelationshipFix2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CampaignUsers",
+                table: "Campaigns");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int[]>(
+                name: "CampaignUsers",
+                table: "Campaigns",
+                type: "integer[]",
+                nullable: true);
+        }
+    }
+}
