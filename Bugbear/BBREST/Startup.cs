527f2e5b7c6cd76b0b40c6392682c19cdb27a46f
Jacob Gaughan <jakegaughan5@gmail.com>
2021-03-31 15:14:52 -0500

    Update Startup.cs

--- Bugbear/BBREST/Startup.cs
+++ Bugbear/BBREST/Startup.cs
@@ -17,6 +17,8 @@ using System.Threading.Tasks;
 using System.Diagnostics.CodeAnalysis;
 using BBREST.Hubs;
 using System.Net.WebSockets;
+using Microsoft.AspNetCore.Http;
+using System.Threading;
 
 namespace BBREST
 {
@@ -82,17 +84,23 @@ namespace BBREST
                 endpoints.MapControllers();
             });
 
+            var webSocketOptions = new WebSocketOptions()
+            {
+                KeepAliveInterval = TimeSpan.FromSeconds(120)
+            };
+            webSocketOptions.AllowedOrigins.Add("https://localhost:4200");
+            webSocketOptions.AllowedOrigins.Add("https://bugbearcampaignmanager.azurewebsites.net");
+
+            app.UseWebSockets(webSocketOptions);
+
             app.Use(async (context, next) =>
             {
-                if (context.Request.Path == "/chatsocket")
+                if (context.Request.Path == "/ws")
                 {
                     if (context.WebSockets.IsWebSocketRequest)
                     {
-                        var socket = await context.WebSockets.AcceptWebSocketAsync();
-                        while (socket.State == WebSocketState.Open)
-                        {
-                            await Task.Delay(TimeSpan.FromMinutes(1));
-                        }
+                        WebSocket webSocket = await context.WebSockets.AcceptWebSocketAsync();
+                        await Echo(context, webSocket);
                     }
                     else
                     {
@@ -104,6 +112,18 @@ namespace BBREST
                     await next();
                 }
             });
+        }
+        private async Task Echo(HttpContext context, WebSocket webSocket)
+        {
+            var buffer = new byte[1024 * 4];
+            WebSocketReceiveResult result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            while (!result.CloseStatus.HasValue)
+            {
+                await webSocket.SendAsync(new ArraySegment<byte>(buffer, 0, result.Count), result.MessageType, result.EndOfMessage, CancellationToken.None);
+
+                result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            }
+            await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
         }
     }
 }
