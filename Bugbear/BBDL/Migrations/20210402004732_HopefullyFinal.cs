6f0f9749528a658d1b43186b4ab86d3535673f60
Jacob Gaughan <jakegaughan5@gmail.com>
2021-04-01 19:49:05 -0500

    TheRealFinal last DB fix

---
+++ Bugbear/BBDL/Migrations/20210402004732_HopefullyFinal.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BBDL.Migrations
+{
+    public partial class HopefullyFinal : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Encounters_Locations_LocationID",
+                table: "Encounters");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Encounters_LocationID",
+                table: "Encounters");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "LocationID",
+                table: "Encounters",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "LocationID",
+                table: "Encounters",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Encounters_LocationID",
+                table: "Encounters",
+                column: "LocationID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Encounters_Locations_LocationID",
+                table: "Encounters",
+                column: "LocationID",
+                principalTable: "Locations",
+                principalColumn: "LocationID",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
